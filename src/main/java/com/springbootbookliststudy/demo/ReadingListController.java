914683ddda5a55546913470d05edb6cf22d17501
gaZero <ppggyyyyy@gmail.com>
2020-04-14 01:19:08 +0900

    test

--- src/main/java/com/springbootbookliststudy/demo/ReadingListController.java
+++ src/main/java/com/springbootbookliststudy/demo/ReadingListController.java
@@ -1,5 +1,6 @@
 package com.springbootbookliststudy.demo;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,18 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/")
 public class ReadingListController {
 
     private static final String reader = "craig";
 
-    private ReadingListRepository readingListRepository;
-
-    @Autowired
-    public ReadingListController(ReadingListRepository readingListRepository){
-        this.readingListRepository=readingListRepository;
-    }
+    private final ReadingListRepository readingListRepository;
 
     @GetMapping
     public String readerBooks(Model model){
