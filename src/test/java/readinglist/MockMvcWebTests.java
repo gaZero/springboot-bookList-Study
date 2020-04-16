da63fb28ed60d46dc9f44fd87bc84f65fbfb0f79
gaZero <ppggyyyyy@gmail.com>
2020-04-17 02:07:30 +0900

    test code

---
+++ src/test/java/readinglist/MockMvcWebTests.java
@@ -0,0 +1,29 @@
+package readinglist;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes=ReadingListApplication.class)
+@WebAppConfiguration
+public class MockMvcWebTests {
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc(){
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webContext)
+                .build();
+    }
+}
